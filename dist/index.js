e69de7d8592a05c0197a3c914b2d4469c6943043
Evan Fleming <evan.gordon.fleming@gmail.com>
2021-02-20 15:54:26 -0800

    adding typescript and publising to npm

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.doSomething = void 0;
+var doSomething = function () { return console.log('Hello World!'); };
+exports.doSomething = doSomething;
